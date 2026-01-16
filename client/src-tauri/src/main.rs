d59f133741f7ca5835db6fabc0171e6ba4d814f1
atar4xis <190674462+atar4xis@users.noreply.github.com>
2026-01-16 03:30:36 +0000

    client: add tauri for desktop app support

---
+++ client/src-tauri/src/main.rs
@@ -0,0 +1,8 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1"); // fixes linux nvidia GPU issue
+
+    app_lib::run();
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, type ConfigEnv } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/acLife/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }: ConfigEnv) => {
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    base: mode === "tauri" ? "./" : "/acLife/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  });
+};
