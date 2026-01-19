981c33587e04e4f5f6147ce22a5ed6720b74dbb6
atar4xis <190674462+atar4xis@users.noreply.github.com>
2026-01-19 00:34:07 +0000

    client(calendar): have drag overlay track itself and not go off screen

--- client/src/components/calendar/DragOverlay.tsx
+++ client/src/components/calendar/DragOverlay.tsx
@@ -1,22 +1,39 @@
 import type { EventDragRef } from "@/types/calendar/Event";
-import type { RefObject } from "react";
+import { useEffect, useRef, useState, type RefObject } from "react";
 
 export default (function DragOverlay({
   dragRef,
 }: {
   dragRef: RefObject<EventDragRef>;
 }) {
-  if (!dragRef.current?.label) return;
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const myRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      setX(e.clientX);
+      setY(e.clientY);
+    };
+
+    document.addEventListener("pointermove", listener);
+    return () => document.removeEventListener("pointermove", listener);
+  }, [setX, setY]);
+
+  if (!dragRef.current?.label) return null;
+
+  const myWidth = myRef.current ? myRef.current.clientWidth : 100;
 
   return (
     <div
+      ref={myRef}
       style={{
         position: "fixed",
-        top: dragRef.current.y + 16,
-        left: dragRef.current.x + 16,
+        top: y - 16,
+        left: x + myWidth > window.innerWidth ? x - myWidth - 16 : x + 16,
         zIndex: 20,
       }}
-      className="bg-background/80 p-1"
+      className="bg-background/80 p-1 truncate"
     >
       {dragRef.current.label}
     </div>
